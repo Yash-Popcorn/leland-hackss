b48e32cf3703834817560393137a7ae6cec98862
Yash Singh <saiansh2525@gmail.com>
2022-09-18 16:33:44 -0700

    fix: login on registger

--- pages/api/register.ts
+++ pages/api/register.ts
@@ -1,6 +1,8 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next';
 import Redis from 'ioredis';
+import { randomUUID } from 'crypto';
+import { setCookie } from 'cookies-next';
 
 const redis = new Redis(9001);
 
@@ -10,6 +12,10 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse<
   }
 
   const { email, password } = req.body;
+
+  const id = randomUUID();
+  await redis.hset(`rid:${id}`, 'id', email);
+  setCookie('rid', id, { req, res });
 
   const existing = await redis.hget(`user:${email}`, 'users');
   if (existing) {
