f7493211af0fa8b0dab7bd0c9488be31dbc109dc
Yash Singh <saiansh2525@gmail.com>
2022-09-18 15:51:40 -0700

    feat: server side props

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,10 +1,16 @@
-import { NextPage } from "next";
+import { getCookie } from 'cookies-next';
+import { GetServerSideProps, NextPage } from 'next';
 
 export const Dashboard: NextPage = () => {
-    
-    return (
-        <div>
-            
-        </div>
-    )
-}
+  return <div></div>;
+};
+
+export const getServerSideProps: GetServerSideProps = async function ({ req, res }) {
+  return {
+    props: {
+      id: getCookie('rid', { req, res }),
+    },
+  };
+};
+
+export default Dashboard;
