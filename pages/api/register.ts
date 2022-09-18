80c191603457b8fc4e49332bca58b60965e3b36a
Yash Singh <saiansh2525@gmail.com>
2022-09-18 13:52:47 -0700

    feat: register route

---
+++ pages/api/register.ts
@@ -0,0 +1,21 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next';
+import Redis from 'ioredis';
+
+const redis = new Redis();
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse<{ error?: string }>) {
+  if (req.method !== 'POST') {
+    return res.status(400).json({ error: 'Method not allowed' });
+  }
+
+  const { email, password } = req.body;
+
+  const existing = await redis.hget(`user:${email}`, 'users');
+  if (existing) {
+    return res.status(500).json({ error: 'User already exists' });
+  }
+  await redis.hset(`user:${email}`, 'users', password);
+
+  res.send({});
+}
