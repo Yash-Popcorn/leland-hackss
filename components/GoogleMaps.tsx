a45b47d4159da19024d50dd411564683be27fdd0
Yash Singh <saiansh2525@gmail.com>
2022-09-18 18:17:44 -0700

    fix: google maps

--- components/GoogleMaps.tsx
+++ components/GoogleMaps.tsx
@@ -1,14 +1,15 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useMemo, useRef } from 'react';
 import { v4 } from 'uuid';
 
 function GoogleMaps() {
-  const [id, _setId] = useState(v4());
+  const reference = useRef<HTMLDivElement | null>(null);
 
   useEffect(function initMap() {
     const interval = setInterval(() => {
-      if (typeof google !== 'undefined') {
+      if (typeof google !== 'undefined' && reference) {
+        clearInterval(interval);
         const uluru = { lat: 37.238768, lng: -121.883805 };
-        const map = new google.maps.Map(document.getElementById(`map-${id}`) as HTMLElement, {
+        const map = new google.maps.Map(reference.current as HTMLElement, {
           zoom: 10,
           center: uluru,
         });
@@ -17,12 +18,11 @@ function GoogleMaps() {
           position: uluru,
           map: map,
         });
-        clearInterval(interval);
       }
     }, 100);
   }, []);
 
-  return <div id={`map-${id}`} style={{ height: '400px', width: '100%' }} />;
+  return <div ref={reference} style={{ height: '400px', width: '100%' }} />;
 }
 
 export default GoogleMaps;
