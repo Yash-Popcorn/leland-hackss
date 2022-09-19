123267345588d91d931b85867dca779a968de8a4
Yash Singh <saiansh2525@gmail.com>
2022-09-18 17:55:10 -0700

    fix: maps..;pls

--- components/GoogleMaps.tsx
+++ components/GoogleMaps.tsx
@@ -3,23 +3,25 @@ import { v4 } from 'uuid';
 
 function GoogleMaps() {
   const [id, _setId] = useState(v4());
-  
+
   useEffect(function initMap() {
-    const uluru = { lat: 37.238768, lng: -121.883805 };
-    const map = new google.maps.Map(
-      document.getElementById(`map-${id}`) as HTMLElement,
-      {
-        zoom: 10,
-        center: uluru,
+    const interval = setInterval(() => {
+      if (typeof google !== 'undefined') {
+        const uluru = { lat: 37.238768, lng: -121.883805 };
+        const map = new google.maps.Map(document.getElementById(`map-${id}`) as HTMLElement, {
+          zoom: 10,
+          center: uluru,
+        });
+
+        const marker = new google.maps.Marker({
+          position: uluru,
+          map: map,
+        });
+        clearInterval(interval);
       }
-    );
-  
-    const marker = new google.maps.Marker({
-      position: uluru,
-      map: map,
-    });
+    }, 100);
   }, []);
-  
+
   return <div id={`map-${id}`} style={{ height: '400px', width: '100%' }} />;
 }
 
