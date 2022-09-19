639b70f0d5564ad35b3a8a7faefa4b7fd56c9414
Yash Singh <saiansh2525@gmail.com>
2022-09-18 17:41:51 -0700

    feat: maps

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -2,38 +2,45 @@ import { getCookie } from 'cookies-next';
 import { GetServerSideProps, NextPage } from 'next';
 import { useRouter } from 'next/router';
 import { useEffect, useState } from 'react';
+import GoogleMaps from '../components/GoogleMaps';
 
 interface dashboard_props {
-    id: string
+  id: string;
 }
 
-export const Dashboard: NextPage<dashboard_props> = ({id}) => {
+export const Dashboard: NextPage<dashboard_props> = ({ id }) => {
+  const router = useRouter();
+  const [email, setEmail] = useState('');
 
-    const router = useRouter()
-    const [email, setEmail] = useState('')
+  useEffect(() => {
+    fetch(`/api/about`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        id,
+      }),
+    })
+      .then((response) => {
+        return response.json();
+      })
+      .then((result: { error: string; email: string }) => {
+        if (result.error) {
+          alert(result.error);
+          router.push(`/`);
+        } else {
+          setEmail(result.email);
+        }
+      });
+  }, []);
 
-    useEffect(() => {
-        fetch(`/api/about`, {
-            method: "POST",
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                id
-            })
-        }).then(response => {
-            return response.json()
-        }).then((result: {error: string, email: string}) => {
-            if (result.error) {
-                alert(result.error)
-                router.push(`/`)
-            } else {
-                setEmail(result.email)
-            }
-        })
-    }, [])
-
-  return <div>Hello, {email}!</div>;
+  return (
+    <div>
+      Hello, {email}!
+      <GoogleMaps />
+    </div>
+  );
 };
 
 export const getServerSideProps: GetServerSideProps = async function ({ req, res }) {
